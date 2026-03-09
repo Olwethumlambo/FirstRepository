61799f22444d0676ac38360db6ead11f77974e69
Olwethumlambo <ST10529421@myemeris.edu.za>
2026-03-09 10:01:30 +0200

    checking conditions
    checking logical conditions

--- app/src/main/java/com/example/logicalop/MainActivity.kt
+++ app/src/main/java/com/example/logicalop/MainActivity.kt
@@ -38,6 +38,63 @@ class MainActivity : AppCompatActivity() {
         tvResult = findViewById(R.id.tvResult)
 
 
+        //button to run the code when it is  pressed
+        btCheck.setOnClickListener {
+
+            //get the user's name from the edtText
+            val name = edtName.text.toString()
+
+            //convert the age entered into a number(int)
+            val age = edtAge.text.toString().toInt()
+
+
+            //check if checkboxes are selected (true or false)
+            val IsITstudent = cbITStudent.isChecked
+            val isStaff = cbStaff.isChecked
+            val isBanned = cbBanned.isChecked
+
+            if (age >= 18 && isITstudent || isStaff && !isBanned) {
+
+                //if the condition is true
+                tvResult.text = "Congratulations $name! You qualify for the student tech discount"
+
+            } else
+
+            //if the condition above is false
+                tvResult.text = "Sorry $name, you do not qualify for trhe discount"
+            }
+
+            /*
+            Logical condition explanation
+
+               Step 1:
+               (age >= && isITStudent
+               >= means greater than or equal to
+               && means AND (both conditions must be true)
+
+               so this checks if the user is 18 or older and is an IT student
+
+               step 2:
+               || means OR (only one condition needs to be true)
+
+               This means the person can qualify if they are a staff member even if they are not
+               an IT student
+
+               step 3: && !isBanned
+               ! means NOT (it reverses the value)
+               !isBanned means the person must NOT be banned
+
+               Example
+               isBanned =false
+               !false = true
+
+
+
+
+
+             */
+
+
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
