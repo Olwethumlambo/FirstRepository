88bc6fcff336b0114b8f6adf0fe2d715c106e6f3
Olwethumlambo <ST10529421@myemeris.edu.za>
2026-03-09 09:17:25 +0200

    linking
    linking the variables to the UI components

--- app/src/main/java/com/example/logicalop/MainActivity.kt
+++ app/src/main/java/com/example/logicalop/MainActivity.kt
@@ -27,6 +27,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+       //link the variables to the UI components using their  ID's (typecasting)
+       edtName = findViewById(R.id.edtName)
+        edtAge = findViewById(R.id.edtAge)
+        cbStaff= findViewById(R.id.cbStaff)
+        cbITStudent = findViewById(R.id.cbITStudent)
+        cbBanned = findViewById(R.id.cbBanned)
+        btCheck = findViewById(R.id.btnCheck)
+        tvResult = findViewById(R.id.tvResult)
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
